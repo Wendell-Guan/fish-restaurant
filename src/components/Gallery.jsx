7533229c32866236f72d3ec2b41bece0f3da1735
Wendell-Guan <266549125+Wendell-Guan@users.noreply.github.com>
2026-04-16 13:46:53 -0700

    Update gallery to feature only unique fresh raw seafood images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -12,25 +12,25 @@ const Gallery = () => {
 
         <div className="gallery-grid">
           <div className="gallery-item item-chef">
-            <img src="/assets/images/gallery_chef.png" alt="Chef flambe" />
+            <img src="/assets/images/gallery_raw_grouper.png" alt="Raw Grouper on ice" />
           </div>
           <div className="gallery-item item-crab">
-            <img src="/assets/images/suriname_red_snapper.png" alt="Suriname Red Snapper" />
+            <img src="/assets/images/gallery_crab.png" alt="Fresh Raw Crab" />
           </div>
           <div className="gallery-item item-school">
-            <img src="/assets/images/suriname_seabob_shrimp.png" alt="Atlantic Seabob Shrimp" />
+            <img src="https://images.unsplash.com/photo-1615141982883-c7ad0e69fd62?q=80&w=800" alt="Fresh market fish display" />
           </div>
           <div className="gallery-item item-squid">
-            <img src="/assets/images/gallery_squid.png" alt="Squid dish" />
-          </div>
-          <div className="gallery-item item-shrimp">
-            <img src="/assets/images/suriname_catfish.png" alt="Suriname Catfish Dish" />
-          </div>
-          <div className="gallery-item item-clams">
             <img src="/assets/images/suriname_croaker.png" alt="Whitemouth Croaker" />
           </div>
+          <div className="gallery-item item-shrimp">
+            <img src="/assets/images/suriname_seabob_shrimp.png" alt="Fresh Seabob Shrimp" />
+          </div>
+          <div className="gallery-item item-clams">
+            <img src="/assets/images/gallery_clams.png" alt="Fresh Raw Clams" />
+          </div>
           <div className="gallery-item item-fish">
-            <img src="/assets/images/hero_fish_plate.png" alt="Raw fish plate" />
+            <img src="https://images.unsplash.com/photo-1521566652839-697aa473761a?q=80&w=800" alt="Fresh sea fish varieties" />
           </div>
         </div>
         
